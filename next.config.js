1ad26bce7e3df6b3069f98e87736fd6bc331e425
Gustavo Richard <gustavo.richard@outlook.com.br>
2022-02-23 15:55:36 -0300

    Botão para login com google

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    mongodburl: "Your MongoDB connection String",
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,36 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useSession, signIn, signOut } from 'next-auth/react'
+import Image from 'next/image'
+
+import { signIn, } from 'next-auth/react'
+
+import { FcGoogle } from 'react-icons/fc'
 
 import styles from '../styles/pages/Login.module.scss'
-import { useEffect, useState } from 'react'
+import logoGoogleUrl from '../assets/btnGoogle.png'
+
 
 const Home: NextPage = () => {
-  const { data: session } = useSession()
-  const router = useRouter();
-
-  // console.log(session?.user)
-
-  // useEffect(() => {
-  //   if (session?.user) 
-  // }, [])
 
   async function handleLogin() {
     signIn('google', { callbackUrl: 'http://localhost:3000/HomePage' })
   }
 
-  // if (session?.user) {
-  //   return (
-  //     <>
-  //       {
-
-  //         /* <p>signed in as {data?.user}</p> <br /> */
-  //       }
-  //       <p>{session.user.name}</p>
-  //       <button onClick={() => signOut()}>Sign out</button>
-  //     </>
-  //   )
-  // }
 
   return (
     <div className={styles.container}>
-
-
       <h1>Login</h1>
-      {/* <Link href='/HomePage'>Logar com e-mail</Link> */}
-      <button onClick={handleLogin}>Sign in</button>
+      <div className={styles.googleButton}>
+        <button onClick={handleLogin}>
+          <p>Entrar com Google</p>
+          <FcGoogle size={20} />
+        </button>
+      </div>
+
+
+
     </div>
   )
 }
 
 export default Home
+
